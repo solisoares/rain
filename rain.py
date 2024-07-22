a8cb2f603a7c14ddf6de19c435c6ff13a4260e66
solisoares <solisoareswa@gmail.com>
2024-07-22 17:13:01 -0300

    Run formatter on source code

--- rain.py
+++ rain.py
@@ -25,7 +25,10 @@ def update_buff(buff: List[List], values: List) -> List[List]:
 
 def print_rain(buff):
     if LOG:
-        print(f"[LOG] ASCII_DROP:{ASCII_DROP} | RAIN_DENSITY:{RAIN_DENSITY} | TICK:{TICK} | WIND:{WIND}", end="")
+        print(
+            f"[LOG] ASCII_DROP:{ASCII_DROP} | RAIN_DENSITY:{RAIN_DENSITY} | TICK:{TICK} | WIND:{WIND}",
+            end="",
+        )
 
     # clouds
     print("""
@@ -38,24 +41,27 @@ def print_rain(buff):
                ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡶⠈⣭⣾⣿⣿⡘⣡⣾⢆⣭⣽⣹⣟⠙⢨⢴⣒⠲⣙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣭⣛⣓⣊⢭⣛⣟⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢀⡨⢼⢨⢯⢷⢿⣮⠦⠼⣿⠿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣾
                ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠚⡀⠘⠾⢵⣙⣻⣿⡿⢃⣿⣿⣿⣿⣿⣿⢩⡶⠦⢄⢺⡇⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣰⣽⣛⠿⣿⣿⣿⣿⣿⣿⣧⣐⢲⠤⣴⠵⠷⢖⣒⣒⣋⣴⣾⣿⣿⣿⣶⣶⣭⣝⡛⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿
                ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣬⣭⣭⣭⣭⣭⣴⣶⣿⣿⣿⣿⣿⣿⣿⣛⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣛⣻⣩⣤⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿""")
-    # rain           
+    # rain
     for line in buff:
         for i in range(0, random.randint(0, 1 if WIND == True else 0)):
             line.insert(0, " ")
             line.pop()
-        print(" "*(15), end="")
+        print(" " * (15), end="")
         for item in line:
             print(item, end=" ")
         print()
 
 
 while True:
-    #TODO: I think this density thing is not well implemented
-    current_values = [ASCII_DROP if random.randint(0, 100) >= (100 - RAIN_DENSITY) else " " for i in range(BUFF_WIDTH)]
+    # TODO: I think this density thing is not well implemented
+    current_values = [
+        ASCII_DROP if random.randint(0, 100) >= (100 - RAIN_DENSITY) else " "
+        for i in range(BUFF_WIDTH)
+    ]
     if len(buff) < BUFF_HEIGHT:
         buff.append(current_values)
     else:
         update_buff(buff, current_values)
         os.system("clear")
-        print_rain( buff)
+        print_rain(buff)
         time.sleep(TICK)
